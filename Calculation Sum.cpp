b60e5f9d1e996d81dde0c7500a6dd32aef000e44
Emmanual Baby <101249626+emmanualbaby@users.noreply.github.com>
2024-09-27 23:35:22 +0530

    Add files via upload

---
+++ Calculation Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int a;
+int b;
+int sum = 0;
+
+cout<<"Enter first number";
+cin>>a;
+
+cout<<"Enter second number";
+cin>>b;
+
+sum = a + b;
+
+cout<<"Sum is"<<sum;
+
+    return 0;
+}
